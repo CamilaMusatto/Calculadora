d820df99cf9c5752541830d0d522623ae95ee76d
Camila <cmusatto@gmail.com>
2022-12-01 13:57:40 -0300

    alteração: teclado 'escuta' valores digitados e display exibe os valores digitados

--- src/main/java/br/com/cmarchi/visao/Teclado.java
+++ src/main/java/br/com/cmarchi/visao/Teclado.java
@@ -1,5 +1,7 @@
 package br.com.cmarchi.visao;
 
+import br.com.cmarchi.modelo.Memoria;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -75,7 +77,7 @@ public class Teclado extends JPanel implements ActionListener {
     public void actionPerformed(ActionEvent e) {
         if(e.getSource() instanceof JButton) {
             JButton botao = (JButton) e.getSource();
-            System.out.println(botao.getText());
+            Memoria.getInstancia().processarComando(botao.getText());
         }
 
     }
